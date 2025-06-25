e0a3eb63662d2d371f65a9fda2466d27ba30aad3
Git <lina@rcelectronic.co.id>
2025-06-25 23:35:40 +0700

    first commit

---
+++ src/main/java/com/mycompany/jtabletest/JTableTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jtabletest;
+
+/**
+ *
+ * @author Putra N. Makatita
+ */
+public class JTableTest {
+
+    public static void main(String[] args) {
+        jTableDisplay.main(args);
+    }
+}
